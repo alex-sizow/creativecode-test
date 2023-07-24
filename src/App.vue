e512d53196e6fd576daef8d77a6c07e3d0e1fe25
Alex <alex_sizow@outlook.com>
2023-07-24 23:36:03 +0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup>
+import LayoutHeader from '@/components/layout/Header.vue';
+</script>
+
+<template><layout-header /></template>
+
+<style lang="scss">
+@import './styles/global.scss';
+</style>
---
+++ src/components/Layout/Header.vue
@@ -0,0 +1,28 @@
+<script setup></script>
+
+<template>
+	<header class="header">
+		<a
+			href="/"
+			class="header__logo"
+			>Uptify</a
+		>
+		<img
+			src="@/assets/buttons/login.svg"
+			alt="SVG file" />
+	</header>
+</template>
+
+<style lang="scss" scoped>
+.header {
+	padding: 24px 32px 0;
+	width: 100%;
+	display: flex;
+	align-items: center;
+	justify-content: space-between;
+	&__logo {
+		font-size: 48px;
+		font-weight: 900;
+	}
+}
+</style>
