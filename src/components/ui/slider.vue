4b1febb6b6e3156f89b84753effb4642a4157c96
Alex <alex_sizow@outlook.com>
2023-08-01 23:55:15 +0300

    swiper pagination styles

--- src/components/ui/slider.vue
+++ src/components/ui/slider.vue
@@ -3,8 +3,10 @@ import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 import 'swiper/css/scrollbar';
+import { Pagination } from 'swiper/modules';
 import { Swiper, SwiperSlide } from 'swiper/vue';
 import BrandBlock from './brandBlock.vue';
+const modules = [Pagination];
 </script>
 
 <template>
@@ -13,6 +15,74 @@ import BrandBlock from './brandBlock.vue';
 			:pagination="true"
 			:modules="modules"
 			class="mySwiper">
+			<swiper-slide>
+				<div class="slide">
+					<brand-block
+						><span>Trusted by</span>
+						<span class="big-text">16</span>
+						<span>dealers</span>
+					</brand-block>
+					<brand-block
+						><img
+							src="../../assets/brands/KennyRoss.svg"
+							alt=""
+					/></brand-block>
+					<brand-block
+						><img
+							src="../../assets/brands/kia.svg"
+							alt=""
+					/></brand-block>
+					<brand-block
+						><img
+							src="../../assets/brands/1Coch.svg"
+							alt=""
+					/></brand-block>
+					<brand-block
+						><img
+							src="../../assets/brands/Honda.svg"
+							alt=""
+					/></brand-block>
+					<brand-block
+						><img
+							src="../../assets/brands/Mid.svg"
+							alt=""
+					/></brand-block>
+				</div>
+			</swiper-slide>
+			<swiper-slide>
+				<div class="slide">
+					<brand-block
+						><span>Trusted by</span>
+						<span class="big-text">16</span>
+						<span>dealers</span>
+					</brand-block>
+					<brand-block
+						><img
+							src="../../assets/brands/KennyRoss.svg"
+							alt=""
+					/></brand-block>
+					<brand-block
+						><img
+							src="../../assets/brands/kia.svg"
+							alt=""
+					/></brand-block>
+					<brand-block
+						><img
+							src="../../assets/brands/1Coch.svg"
+							alt=""
+					/></brand-block>
+					<brand-block
+						><img
+							src="../../assets/brands/Honda.svg"
+							alt=""
+					/></brand-block>
+					<brand-block
+						><img
+							src="../../assets/brands/Mid.svg"
+							alt=""
+					/></brand-block>
+				</div>
+			</swiper-slide>
 			<swiper-slide>
 				<div class="slide">
 					<brand-block
